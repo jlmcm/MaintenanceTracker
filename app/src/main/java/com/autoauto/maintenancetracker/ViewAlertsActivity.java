fbe1e4b4545cb1c52155a8344beffc5fc0d47887
dexyr <61259802+Dexyr@users.noreply.github.com>
2020-04-14 10:38:13 -0500

    I changed the UI element for dismissing alerts the title can be removed (basically replaced with a label in the layout
    still need to hook up alerts to android notifications
    
    still need to do debugging with the actual device

--- app/src/main/java/com/autoauto/maintenancetracker/ViewAlertsActivity.java
+++ app/src/main/java/com/autoauto/maintenancetracker/ViewAlertsActivity.java
@@ -8,7 +8,9 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.autoauto.maintenancetracker.util.MaintenanceScheduler;
@@ -40,6 +42,32 @@ public class ViewAlertsActivity extends AutoAutoActivity {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Dismiss Alert");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.dismiss_alert_dialog, null);
+
+        // performed button
+        Button btPerformed = view.findViewById(R.id.btPerformed);
+        btPerformed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Performed", Toast.LENGTH_SHORT).show();
+                DismissAlert(position);
+            }
+        });
+
+        // skipped button
+        Button btSkipped = view.findViewById(R.id.btSkipped);
+        btSkipped.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Skipped", Toast.LENGTH_SHORT).show();
+                DismissAlert(position);
+            }
+        });
+
+        builder.setView(view);
+        /*
         builder.setItems(dismissReasons, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
@@ -47,11 +75,13 @@ public class ViewAlertsActivity extends AutoAutoActivity {
                 context.DismissAlert(position);
             }
         });
+         */
         AlertDialog dismissAlert = builder.create();
         dismissAlert.show();
     }
 
     public void DismissAlert(int position) {
+        // need to attach the reason here
         MaintenanceScheduler maintenanceScheduler = application.getVehicle().getMaintenanceScheduler();
         
         maintenanceScheduler.ExpireTask(position);
